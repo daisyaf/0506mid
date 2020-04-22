3e21d3c9c6c1ebe745a9adc95b2b9cee3aa99080
cflin <cfiin@mail.cjcu.edu.tw>
2020-04-22 14:37:17 +0800

    做多國語系

--- app/src/main/java/com/example/test_bmi/Cal_BMI.java
+++ app/src/main/java/com/example/test_bmi/Cal_BMI.java
@@ -3,13 +3,23 @@ package com.example.test_bmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class Cal_BMI extends AppCompatActivity {
+
+    private TextView tv_name;
+    private ImageView iv_pic;
+    private TextView tv_msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cal__bmi);
+
+        findViews();
 
         Bundle bundle = getIntent().getExtras();
         String name = bundle.getString("name");
@@ -19,6 +29,7 @@ public class Cal_BMI extends AppCompatActivity {
         double h = Double.parseDouble(height) /100.0;
         double w = Double.parseDouble(weight);
 
+        DecimalFormat df = new DecimalFormat("#.##");
         double bmi = w / (h *h);
 
         String msg = "";
@@ -29,7 +40,13 @@ public class Cal_BMI extends AppCompatActivity {
         } else {
             msg = "標準";
         }
+        tv_name.setText(name);
+        tv_msg.setText("你的BMI是"+df.format(bmi) +","+msg);
+    }
 
-
+    private void findViews() {
+        tv_name = (TextView) findViewById(R.id.tv_name);
+        iv_pic = (ImageView) findViewById(R.id.iv_pic);
+        tv_msg = (TextView) findViewById(R.id.tv_msg);
     }
 }
